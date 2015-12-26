126522feb1c6709e3b1a5ae21cf4d6e5ad47ba80
Diomidis Spinellis <dds@aueb.gr>
2015-12-27 01:26:52 +0200

    Add assembly language example

---
+++ asm/loop.asm
@@ -0,0 +1,36 @@
+_DATA	SEGMENT				; Data area
+$SG2748	DB	'%d', 0aH, 00H  	; printf format string
+_DATA	ENDS
+
+PUBLIC	_main				; Export main
+EXTRN	_printf:PROC			; Import printf
+
+_TEXT	SEGMENT				; Code area
+_i$ = -4 				; Stack offset where i is stored
+_main	PROC
+	push	ebp			; Function entry boilerplate
+	mov	ebp, esp
+	push	ecx
+	mov	DWORD PTR _i$[ebp], 0	; i = 0
+	jmp	SHORT $LN3@main		; Jump to loop's end
+$LN2@main:				; Loop's top label
+	mov	eax, DWORD PTR _i$[ebp]	; Get i into register eax
+	add	eax, 1			; Increment by one
+	mov	DWORD PTR _i$[ebp], eax	; Store eax back to i
+$LN3@main:				; Loop's end label
+	cmp	DWORD PTR _i$[ebp], 10 	; Compare i to 10
+	jge	SHORT $LN1@main		; If greater or equal terminate loop
+	mov	ecx, DWORD PTR _i$[ebp]	; Get i into register ecx
+	push	ecx			; Push ecx as argument to printf
+	push	OFFSET $SG2748		; Push printf format string argument
+	call	_printf			; Call printf
+	add	esp, 8			; Free pushed printf arguments
+	jmp	SHORT $LN2@main		; Jump to loop's top
+$LN1@main:				; Loop's exit label
+	xor	eax, eax		; Zero eax as main's return value
+	mov	esp, ebp		; Function exit boilerplate
+	pop	ebp
+	ret	0
+_main	ENDP
+_TEXT	ENDS
+END
---
+++ asm/loop.s
@@ -0,0 +1,29 @@
+	.section	.rodata		@ Data area
+	.align	2			@ Align on even memory address
+.LC0:	.ascii	"%d\012\000"		@ Printf format string
+	.text				@ Code area
+	.global	main			@ Export main
+main:					@ Entry point of main
+	stmfd	sp!, {fp, lr}		@ Function entry boilerplate
+	add	fp, sp, #4
+	sub	sp, sp, #8
+	mov	r3, #0			@ Set register r3 to zero
+	str	r3, [fp, #-8]		@ Store register r3 into i
+	b	.L2			@ Branch to loop's end
+.L3:					@ Loop's top label
+	ldr	r3, .L4			@ Get printf format address
+	mov	r0, r3			@ Set format as first argument
+	ldr	r1, [fp, #-8]		@ Set i as second argument
+	bl	printf			@ Call printf
+	ldr	r3, [fp, #-8]		@ Get i into register r3
+	add	r3, r3, #1		@ Increment r3 by one
+	str	r3, [fp, #-8]		@ Store register r3 into i
+.L2:					@ Loop's end label
+	ldr	r3, [fp, #-8]		@ Get i into register r3
+	cmp	r3, #9			@ Compare r3 with 9
+	ble	.L3			@ Branch if less or equal to loop's top
+	mov	r3, #0			@ Set r3 to zero
+	mov	r0, r3			@ Zero r0 as main's return value
+	sub	sp, fp, #4		@ Function exit boilerplate
+	ldmfd	sp!, {fp, pc}
+.L4:	.word	.LC0			@ Address of printf format arg
