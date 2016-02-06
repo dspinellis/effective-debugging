d035a20ab576ea104d652e6449b22ed0f9b2fb90
Diomidis Spinellis <dds@aueb.gr>
2016-02-06 16:36:14 +0200

    Add code for reverse debugging example

---
+++ reverse/README
@@ -0,0 +1,1 @@
+Simple program to demonstrate reverse debugging
---
+++ reverse/loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int
+main()
+{
+        int i, sum = 0;
+
+        for (i = 0; i < 10; i++)
+                sum += i;
+        printf("%d\n", sum);
+}
