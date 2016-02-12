6d8bdaead07688d05d05d50ee9ccd9e5eb7a6c09
Diomidis Spinellis <dds@aueb.gr>
2016-02-12 17:54:19 +0200

    Add false sharing examples

---
+++ false-share/sum-mp-noshare.c
@@ -0,0 +1,22 @@
+#include <omp.h>
+
+#define N 100000000
+#define NTHREADS 8
+
+int
+main(int argc, char *argv[])
+{
+    int tid;
+    static int values[N];
+    static int sum[NTHREADS];
+
+    omp_set_num_threads(NTHREADS);
+#pragma omp parallel private(tid)
+    {
+	int local_sum = 0;
+	tid = omp_get_thread_num();
+	for (int i = 0; i < N; i++)
+	    local_sum += values[i];
+	sum[tid] = local_sum;
+    }
+}
---
+++ false-share/sum.c
@@ -0,0 +1,24 @@
+#include <omp.h>
+
+#define N 100000000
+#define NTHREADS 8
+
+int
+main(int argc, char *argv[])
+{
+    int tid;
+    static int values[N];
+    static int sum[NTHREADS];
+
+#ifdef _OPENMP
+    omp_set_num_threads(NTHREADS);
+#pragma omp parallel private(tid)
+    {
+	tid = omp_get_thread_num();
+#else
+    for (tid = 0; tid < NTHREADS; tid++) {
+#endif
+	for (int i = 0; i < N; i++)
+	    sum[tid] += values[i];
+    }
+}
