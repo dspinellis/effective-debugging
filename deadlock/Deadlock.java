bd16eaae103f65e7c77180f5764add650a3ec01f
Diomidis Spinellis <dds@aueb.gr>
2016-02-15 18:41:09 +0200

    Add Java example

---
+++ deadlock/Deadlock.java
@@ -0,0 +1,36 @@
+public class Deadlock {
+    public static void main(String args[]) {
+        Object mutex1 = new Object();
+        Object mutex2 = new Object();
+
+        Runnable bob = () -> {
+            for (int i = 0; i < 1000; i++)
+                synchronized(mutex1) {
+                    synchronized(mutex2) {
+                        System.out.println("Hi, it's Bob " + i);
+                    }
+                }
+        };
+        Runnable alice = () -> {
+            for (int i = 0; i < 1000; i++)
+                synchronized(mutex2) {
+                    synchronized(mutex1) {
+                        System.out.println("Hi, it's Alice " + i);
+                    }
+                }
+        };
+
+        Thread at = new Thread(alice);
+        Thread bt = new Thread(bob);
+
+        bt.start();
+        at.start();
+
+        try {
+            at.join();
+            bt.join();
+        } catch(InterruptedException e) {
+            System.err.println("Interrupted: " + e);
+        }
+    }
+}
--- deadlock/README.md
+++ deadlock/README.md
@@ -1,3 +1,4 @@
 Programs that deadlock
 deadlock.cpp: C++ 11
 deadlock-pt.c: POSIX Threads
+Deadlock.java: Java
