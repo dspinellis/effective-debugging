892dd3a99039eb197124d82f56c037d690413baf
Diomidis Spinellis <dds@aueb.gr>
2016-02-17 09:51:32 +0200

    Change task: XHTML validation

--- lock-contention/LockContention.java
+++ lock-contention/LockContention.java
@@ -1,34 +1,40 @@
-import java.util.Random;
+import java.io.File;
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.*;
+import java.util.concurrent.*;
 
 public class LockContention {
-    static Random randomGen = new Random();
     static int NTHREAD = 7;
-    static Object lock = new Object();
-    static int counter = 0;
+    static Integer counter = new Integer(0);
+    static int numberOfDocuments = 100000;
 
     static public void main(String args[]) {
+        int nThreads = Integer.parseInt(args[0]);
+        int nTasks = Integer.parseInt(args[1]);
         Runnable task = () -> {
-            for (int i = 0; i < 100000; i++)
-                synchronized (lock) {
-                    try {
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-                        System.err.println("Interrupted sleep: " + e);
-                    }
-                    counter++;
-                }
+            File schemaFile = new File("xhtml1-transitional.xsd");
+            Source xmlFile = new StreamSource(new File("w3c.xml"));
+            SchemaFactory schemaFactory = SchemaFactory
+                .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            try {
+                Schema schema = schemaFactory.newSchema(schemaFile);
+                Validator validator = schema.newValidator();
+                validator.validate(xmlFile);
+            } catch (Exception e) {
+              System.out.println(xmlFile.getSystemId() + " failed");
+              System.out.println("Reason: " + e.getLocalizedMessage());
+            }
         };
-
-        Thread threads[] = new Thread[NTHREAD];
-        for (int i = 0; i < NTHREAD; i++) {
-            threads[i] = new Thread(task);
-            threads[i].start();
-        }
+        ExecutorService executor = Executors.newFixedThreadPool(nThreads);
+        for (int i = 0; i < nTasks; i++)
+            executor.submit(task);
         try {
-            for (Thread t : threads)
-                t.join();
+            executor.shutdown();
+            executor.awaitTermination(5, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
-            System.err.println("Interrupted join: " + e);
+            System.err.println("Interrupted await: " + e);
         }
     }
 }
