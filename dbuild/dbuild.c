702f66a4e9ca0ccdb23355fb3fa5837c799913eb
Diomidis Spinellis <dds@aueb.gr>
2016-02-17 19:01:04 +0200

    Correct and clarify

--- dbuild/dbuild.c
+++ dbuild/dbuild.c
@@ -6,9 +6,9 @@ int a;
 int
 main(int argc, char *arg[])
 {
-	printf("stack:\t%p\n", &argc);
+	printf("stack:\t%p\n", (void *)&argc);
 	printf("heap:\t%p\n", malloc(1));
-	printf("text:\t%p\n", main);
-	printf("bss:\t%p\n", &a);
+	printf("code:\t%p\n", (void *)main);
+	printf("data:\t%p\n", (void *)&a);
 	return 0;
 }
