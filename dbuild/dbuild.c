51b2f0503c63d3234ce0d02cf3436d0f76a49d15
Diomidis Spinellis <dds@aueb.gr>
2016-02-18 17:01:32 +0200

    Show various types of data

--- dbuild/dbuild.c
+++ dbuild/dbuild.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int a;
+int z;
+int i = 1;
+const int c = 1;
 
 int
 main(int argc, char *arg[])
@@ -9,6 +11,8 @@ main(int argc, char *arg[])
 	printf("stack:\t%p\n", (void *)&argc);
 	printf("heap:\t%p\n", malloc(1));
 	printf("code:\t%p\n", (void *)main);
-	printf("data:\t%p\n", (void *)&a);
+	printf("data:\t%p (initialized)\n", (void *)&i);
+	printf("data:\t%p (constants)\n", (void *)&c);
+	printf("data:\t%p (zero)\n", (void *)&z);
 	return 0;
 }
