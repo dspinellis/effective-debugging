157bfbb1e74d8faa6a774dce9293c1ce8b63d4f8
Diomidis Spinellis <dds@aueb.gr>
2016-02-18 08:41:37 +0200

    Fix indentation for columns=fixed

--- dlib/stl/intersection.cpp
+++ dlib/stl/intersection.cpp
@@ -5,10 +5,11 @@
 int
 main()
 {
-	std::vector<int> s1 = {5, 3, 2};
-	std::vector<int> s2 = {1, 3, 2};
-	std::vector<int> result;
+    std::vector<int> s1 = {5, 3, 2};
+    std::vector<int> s2 = {1, 3, 2};
+    std::vector<int> result;
 
-	std::set_intersection(s1.begin(), s1.end(),
-		s2.begin(), s2.end(), std::back_inserter(result));
+    std::set_intersection(s1.begin(), s1.end(),
+                          s2.begin(), s2.end(),
+                          std::back_inserter(result));
 }
